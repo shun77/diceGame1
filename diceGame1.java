825ba5fc39d2e17cc5f02359d9f76129e2a23263
shun77 <86811082+shun77@users.noreply.github.com>
2021-07-02 16:43:09 +0900

    Won/lost message added

--- diceGame1.java
+++ diceGame1.java
@@ -11,6 +11,11 @@ public class diceGame1 {
         System.out.println("Die 1 : " + dice1 );
         System.out.println("Die 2 : " + dice2 );
         System.out.println("Total value : " + (dice1 + dice2));
+        if((dice1 + dice2) > 7) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost!");
+        }
        
     }
 }
